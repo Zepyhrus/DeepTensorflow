d4161b4fad5d7bd9e6975b259d2a07c5abad58e9
Zepyhrus <sherkfung@gmail.com>
2020-08-20 10:35:45 +0800

    add mnist pipeline

--- graph.py
+++ graph.py
@@ -3,4 +3,16 @@ import tensorflow as tf
 
 
 if __name__ == "__main__":
-  pass
+  g1 = tf.Graph()
+  
+  with g1.as_default():
+    # 
+    a = tf.Variable(0, name='a')
+
+    assert a.graph is g1
+
+  with tf.Graph().as_default() as g2:
+    # 
+    b = tf.Variable(0, name='b')
+
+    assert b.graph is g2
--- mnist.py
+++ mnist.py
@@ -21,7 +21,21 @@ def main():
   y_ = tf.placeholder(tf.float32, [None, 10])
 
   # 使用交叉熵作为损失值
-  
+  cross_entropy = tf.reduce_mean(
+    tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y)
+  )
+
+  # 如果不使用合并的交叉熵及softmax则
+  _ot = tf.softmax(y)
+  ce = tf.reduce_mean(
+    -tf.reduce_sum(y_ * tf.log(_ot), reduction_indices=[1])
+  )
+
+  # 创建梯度下降优化器
+  optimizer = tf.train.GradientDescentOptimizer(FLAGS.learning_rate)
+
+  # 定义单步训练操作
+  train_op = optimizer.minimize(cross_entropy)
 
 
 if __name__ == "__main__":
